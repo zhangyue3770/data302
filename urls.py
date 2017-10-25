7094b17dbe5029745d2087b80c8fbbdf64510557
Greg Caporaso <gregcaporaso@gmail.com>
2017-10-25 12:11:38 -0400

    MAINT: adds sequences for filtering tutorial (#37)

--- urls.py
+++ urls.py
@@ -850,6 +850,8 @@ MAP = {
         'https://s3-us-west-2.amazonaws.com/qiime2-data/2017.10/tutorials/filtering/table.qza',
     '2017.10/tutorials/filtering/taxonomy.qza':
         'https://s3-us-west-2.amazonaws.com/qiime2-data/2017.10/tutorials/filtering/taxonomy.qza',
+    '2017.10/tutorials/filtering/sequences.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2017.10/tutorials/filtering/sequences.qza',
     '2017.10/tutorials/fmt/fmt-tutorial-demux-1-10p.qza':
         'https://s3-us-west-2.amazonaws.com/qiime2-data/2017.10/tutorials/fmt/fmt-tutorial-demux-1-10p.qza',
     '2017.10/tutorials/fmt/fmt-tutorial-demux-1-1p.qza':
