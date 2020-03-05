f748c6f14a23e1d54a1b86a5a14d8a72b8813732
Matthew Dillon <matthewrdillon@gmail.com>
2020-03-05 13:34:36 -0700

    BUMP: 2020.5

--- urls/_2020.py
+++ urls/_2020.py
@@ -19,6 +19,16 @@ MAP_2020 = {
     'distro/core/qiime20202-BUILDID.zip':
         'https://google.com',
 
+    # 2020.5 DISTRO
+    'distro/core/qiime2-2020.5-py36-osx-conda.yml':
+        'https://raw.githubusercontent.com/qiime2/environment-files/master/2020.5/release/qiime2-2020.5-py36-osx-conda.yml',
+    'distro/core/qiime2-2020.5-py36-linux-conda.yml':
+        'https://raw.githubusercontent.com/qiime2/environment-files/master/2020.5/release/qiime2-2020.5-py36-linux-conda.yml',
+    'distro/core/2020.5':
+        'https://google.com',
+    'distro/core/qiime20205-BUILDID.zip':
+        'https://google.com', 
+
     # 2020.2
     '2020.2/common/gg-13-8-99-515-806-nb-classifier.qza':
         'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.2/common/gg-13-8-99-515-806-nb-classifier.qza',
@@ -206,4 +216,192 @@ MAP_2020 = {
         'https://docs.google.com/spreadsheets/d/1EIjwTSWoBjm3HAjuM0oo5VLA6whVxS6YCzVOJE2y3eU/edit?usp=sharing',
     '2020.2/tutorials/pd-mice/sample_metadata.tsv':
         'https://docs.google.com/spreadsheets/d/e/2PACX-1vTUKT61yWMqo0xLFST1cYnIUKOvDm5YMk7i1h-MiJHU84cWkQ8ehvR3xPATMD21ZGkguNiywN98JWG5/pub?gid=1509704122&single=true&output=tsv',
+
+    # 2020.5
+    '2020.5/common/gg-13-8-99-515-806-nb-classifier.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/common/gg-13-8-99-515-806-nb-classifier.qza',
+    '2020.5/common/gg-13-8-99-nb-classifier.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/common/gg-13-8-99-nb-classifier.qza',
+    '2020.5/common/silva-132-99-515-806-nb-classifier.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/common/silva-132-99-515-806-nb-classifier.qza',
+    '2020.5/common/silva-132-99-nb-classifier.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/common/silva-132-99-nb-classifier.qza',
+    '2020.5/common/sepp-refs-gg-13-8.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/common/sepp-refs-gg-13-8.qza',
+    '2020.5/common/sepp-refs-silva-128.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/common/sepp-refs-silva-128.qza',
+    '2020.5/tutorials/atacama-soils/10p/barcodes.fastq.gz':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/atacama-soils/10p/barcodes.fastq.gz',
+    '2020.5/tutorials/atacama-soils/10p/forward.fastq.gz':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/atacama-soils/10p/forward.fastq.gz',
+    '2020.5/tutorials/atacama-soils/10p/reverse.fastq.gz':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/atacama-soils/10p/reverse.fastq.gz',
+    '2020.5/tutorials/atacama-soils/1p/barcodes.fastq.gz':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/atacama-soils/1p/barcodes.fastq.gz',
+    '2020.5/tutorials/atacama-soils/1p/forward.fastq.gz':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/atacama-soils/1p/forward.fastq.gz',
+    '2020.5/tutorials/atacama-soils/1p/reverse.fastq.gz':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/atacama-soils/1p/reverse.fastq.gz',
+    '2020.5/tutorials/chimera/atacama-table.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/chimera/atacama-table.qza',
+    '2020.5/tutorials/chimera/atacama-rep-seqs.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/chimera/atacama-rep-seqs.qza',
+    '2020.5/tutorials/exporting/feature-table.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/exporting/feature-table.qza',
+    '2020.5/tutorials/exporting/unrooted-tree.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/exporting/unrooted-tree.qza',
+    '2020.5/tutorials/filtering/distance-matrix.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/filtering/distance-matrix.qza',
+    '2020.5/tutorials/filtering/table.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/filtering/table.qza',
+    '2020.5/tutorials/filtering/taxonomy.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/filtering/taxonomy.qza',
+    '2020.5/tutorials/filtering/sequences.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/filtering/sequences.qza',
+    '2020.5/tutorials/fmt/fmt-tutorial-demux-1-10p.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/fmt/fmt-tutorial-demux-1-10p.qza',
+    '2020.5/tutorials/fmt/fmt-tutorial-demux-1-1p.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/fmt/fmt-tutorial-demux-1-1p.qza',
+    '2020.5/tutorials/fmt/fmt-tutorial-demux-2-10p.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/fmt/fmt-tutorial-demux-2-10p.qza',
+    '2020.5/tutorials/fmt/fmt-tutorial-demux-2-1p.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/fmt/fmt-tutorial-demux-2-1p.qza',
+    '2020.5/tutorials/fmt-cdiff-khanna/manifest.csv':
+        'https://qiime2-data.s3-us-west-2.amazonaws.com/2020.5/tutorials/fmt-cdiff-khanna/manifest.csv',
+    '2020.5/tutorials/fmt-cdiff-khanna/sequence_files.zip':
+        'https://qiime2-data.s3-us-west-2.amazonaws.com/2020.5/tutorials/fmt-cdiff-khanna/sequence_files.zip',
+    '2020.5/tutorials/gneiss/sample-metadata.tsv':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/gneiss/sample-metadata.tsv',
+    '2020.5/tutorials/gneiss/table.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/gneiss/table.qza',
+    '2020.5/tutorials/gneiss/taxa.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/gneiss/taxa.qza',
+    '2020.5/tutorials/importing/aligned-sequences.fna':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/importing/aligned-sequences.fna',
+    '2020.5/tutorials/importing/casava-18-paired-end-demultiplexed.zip':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/importing/casava-18-paired-end-demultiplexed.zip',
+    '2020.5/tutorials/importing/casava-18-single-end-demultiplexed.zip':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/importing/casava-18-single-end-demultiplexed.zip',
+    '2020.5/tutorials/importing/feature-table-v100.biom':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/importing/feature-table-v100.biom',
+    '2020.5/tutorials/importing/feature-table-v210.biom':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/importing/feature-table-v210.biom',
+    '2020.5/tutorials/importing/pe-64-manifest':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/importing/pe-64-manifest',
+    '2020.5/tutorials/importing/pe-64.zip':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/importing/pe-64.zip',
+    '2020.5/tutorials/importing/se-33-manifest':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/importing/se-33-manifest',
+    '2020.5/tutorials/importing/se-33.zip':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/importing/se-33.zip',
+    '2020.5/tutorials/importing/sequences.fna':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/importing/sequences.fna',
+    '2020.5/tutorials/importing/unrooted-tree.tre':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/importing/unrooted-tree.tre',
+    '2020.5/tutorials/longitudinal/ecam_table_taxa.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/longitudinal/ecam_table_taxa.qza',
+    '2020.5/tutorials/longitudinal/ecam_shannon.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/longitudinal/ecam_shannon.qza',
+    '2020.5/tutorials/longitudinal/unweighted_unifrac_distance_matrix.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/longitudinal/unweighted_unifrac_distance_matrix.qza',
+    '2020.5/tutorials/longitudinal/ecam_table_maturity.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/longitudinal/ecam_table_maturity.qza',
+    '2020.5/tutorials/moving-pictures/emp-single-end-sequences/barcodes.fastq.gz':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/moving-pictures/emp-single-end-sequences/barcodes.fastq.gz',
+    '2020.5/tutorials/moving-pictures/emp-single-end-sequences/sequences.fastq.gz':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/moving-pictures/emp-single-end-sequences/sequences.fastq.gz',
+    '2020.5/tutorials/metadata/faith_pd_vector.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/metadata/faith_pd_vector.qza',
+    '2020.5/tutorials/metadata/rep-seqs.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/metadata/rep-seqs.qza',
+    '2020.5/tutorials/metadata/taxonomy.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/metadata/taxonomy.qza',
+    '2020.5/tutorials/metadata/unweighted_unifrac_pcoa_results.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/metadata/unweighted_unifrac_pcoa_results.qza',
+    '2020.5/tutorials/otu-clustering/seqs.fna':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/otu-clustering/seqs.fna',
+    '2020.5/tutorials/otu-clustering/85_otus.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/otu-clustering/85_otus.qza',
+    '2020.5/tutorials/pd-mice/animal_distal_gut.qza':
+        'https://qiime2-data.s3-us-west-2.amazonaws.com/2020.5/tutorials/pd-mice/animal_distal_gut.qza',
+    '2020.5/tutorials/pd-mice/demultiplexed_seqs.zip':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/pd-mice/demultiplexed_seqs.zip',
+    '2020.5/tutorials/pd-mice/manifest':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/pd-mice/manifest',
+    '2020.5/tutorials/pd-mice/ref_seqs_v4.qza':
+        'https://qiime2-data.s3-us-west-2.amazonaws.com/2020.5/tutorials/pd-mice/ref_seqs_v4.qza',
+    '2020.5/tutorials/pd-mice/ref_tax.qza':
+        'https://qiime2-data.s3-us-west-2.amazonaws.com/2020.5/tutorials/pd-mice/ref_tax.qza',
+    '2020.5/tutorials/quality-control/qc-mock-3-expected.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/quality-control/qc-mock-3-expected.qza',
+    '2020.5/tutorials/quality-control/qc-mock-3-observed.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/quality-control/qc-mock-3-observed.qza',
+    '2020.5/tutorials/quality-control/query-seqs.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/quality-control/query-seqs.qza',
+    '2020.5/tutorials/quality-control/reference-seqs.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/quality-control/reference-seqs.qza',
+    '2020.5/tutorials/quality-control/query-table.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/quality-control/query-table.qza',
+    '2020.5/tutorials/read-joining/atacama-seqs.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/read-joining/atacama-seqs.qza',
+    '2020.5/tutorials/read-joining/fj-joined.zip':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/read-joining/fj-joined.zip',
+    '2020.5/tutorials/sample-classifier/atacama-table.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/sample-classifier/atacama-table.qza',
+    '2020.5/tutorials/sample-classifier/moving-pictures-table.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/sample-classifier/moving-pictures-table.qza',
+    '2020.5/tutorials/training-feature-classifiers/85_otu_taxonomy.txt':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/training-feature-classifiers/85_otu_taxonomy.txt',
+    '2020.5/tutorials/training-feature-classifiers/85_otus.fasta':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/training-feature-classifiers/85_otus.fasta',
+    '2020.5/tutorials/training-feature-classifiers/rep-seqs.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/2020.5/tutorials/training-feature-classifiers/rep-seqs.qza',
+    '2020.5/tutorials/phylogeny/rep-seqs.qza':
+        'https://qiime2-data.s3-us-west-2.amazonaws.com/2020.5/tutorials/phylogeny/rep-seqs.qza',
+    '2020.5/tutorials/utilities/faith-pd-vector.qza':
+        'https://qiime2-data.s3-us-west-2.amazonaws.com/2020.5/tutorials/utilities/faith-pd-vector.qza',
+    '2020.5/tutorials/utilities/jaccard-pcoa.qza':
+        'https://qiime2-data.s3-us-west-2.amazonaws.com/2020.5/tutorials/utilities/jaccard-pcoa.qza',
+    '2020.5/tutorials/utilities/taxa-barplot.qzv':
+        'https://qiime2-data.s3-us-west-2.amazonaws.com/2020.5/tutorials/utilities/taxa-barplot.qzv',
+
+    # Sample Metadata (hosted on Google Sheets)
+
+    ## FMT
+    '2020.5/tutorials/fmt/sample_metadata':
+        'https://docs.google.com/spreadsheets/d/19rReXhmTT-UXPw4qDzH3bIGZ3xE2EaWQ4AX6w78E19k/edit?usp=sharing',
+    '2020.5/tutorials/fmt/sample_metadata.tsv':
+        'https://docs.google.com/spreadsheets/d/19rReXhmTT-UXPw4qDzH3bIGZ3xE2EaWQ4AX6w78E19k/export?gid=0&format=tsv',
+
+    ## Moving Pictures
+    '2020.5/tutorials/moving-pictures/sample_metadata':
+        'https://docs.google.com/spreadsheets/d/1zCErEpDf5JCXoMmDQWYToIPn07vWdsyKg4UbFc2JgLA/edit?usp=sharing',
+    '2020.5/tutorials/moving-pictures/sample_metadata.tsv':
+        'https://docs.google.com/spreadsheets/d/1zCErEpDf5JCXoMmDQWYToIPn07vWdsyKg4UbFc2JgLA/export?gid=0&format=tsv',
+
+    ## Atacama
+    '2020.5/tutorials/atacama-soils/sample_metadata':
+        'https://docs.google.com/spreadsheets/d/1I2UPNUSJrULSjLa42Dp1cAjvtgEEmK9MAgQyAMBdZ90/edit?usp=sharing',
+    '2020.5/tutorials/atacama-soils/sample_metadata.tsv':
+        'https://docs.google.com/spreadsheets/d/1I2UPNUSJrULSjLa42Dp1cAjvtgEEmK9MAgQyAMBdZ90/export?gid=0&format=tsv',
+
+    ## The following tutorials are the "weird" ones, they use the *new* docs sharing menu, via "File -> Publish to the Web" dialog for TSV export.
+
+    ## Longitudinal
+    '2020.5/tutorials/longitudinal/sample_metadata':
+        'https://docs.google.com/spreadsheets/d/18tYREhE5HcRVTCX3740KXeBOTL7duf_7EeU3MpF7DlE/edit?usp=sharing',
+    '2020.5/tutorials/longitudinal/sample_metadata.tsv':
+        'https://docs.google.com/spreadsheets/d/e/2PACX-1vR6EuKUDh2208iwVW1QZ3LLf_QQGwAYB44mhd84BxIIcOLI7aRxNH4m2bS3bDwr6hGjUpDPBfOeoXyN/pub?gid=1303657428&single=true&output=tsv',
+
+    ## FMT Cdiff
+    '2020.5/tutorials/fmt-cdiff-khanna/sample_metadata':
+        'https://docs.google.com/spreadsheets/d/1Kz6pYslJwbZ75mVcuDkJ6RK9KA0Y205x2958DMBtp2w/edit?usp=sharing',
+    '2020.5/tutorials/fmt-cdiff-khanna/sample_metadata.tsv':
+        'https://docs.google.com/spreadsheets/d/e/2PACX-1vTv3HpYXSClxj1jMoFypgKZKMOjdRQ76JhPHaXvQk6-rgnbh4kBGDZyEPjUwhIj-s59j2EVIOGdA2Kz/pub?gid=283132897&single=true&output=tsv',
+
+    ## PD Mice
+    '2020.5/tutorials/pd-mice/sample_metadata':
+        'https://docs.google.com/spreadsheets/d/1y8__mN4f58D2JCpz2X6mEVQlDYRXnvxhl5A5MPy7nAM/edit?usp=sharing',
+    '2020.5/tutorials/pd-mice/sample_metadata.tsv':
+        'https://docs.google.com/spreadsheets/d/e/2PACX-1vSrf5DfuqbDrn2KgX_A2L5oxrGd2-gWPCGoJlvJKsQa1qQai08-C4HHHI7d_sSM_Sr_OcBJqUjJJLUm/pub?gid=1509704122&single=true&output=tsv',
 }
