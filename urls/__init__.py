271ff2eac694b288dd13670b49fe411740247e38
Chris Keefe <39198770+ChrisKeefe@users.noreply.github.com>
2020-08-28 13:24:34 -0700

    NEW: skbio alpha div metrics (#98)

--- urls/__init__.py
+++ urls/__init__.py
@@ -9,17 +9,15 @@
 # flake8: noqa
 
 from ._old import MAP_OLD
+from ._permanent import MAP_PERMANENT
 from ._2019 import MAP_2019
 from ._2020 import MAP_2020
 
 
 MAP = {
-    'distro/core/aws-amis.txt':
-        'https://s3-us-west-2.amazonaws.com/qiime2-data/distro/core/aws-amis.txt',
-    'distro/core/virtualbox-images.txt':
-        'https://s3-us-west-2.amazonaws.com/qiime2-data/distro/core/virtualbox-images.txt',
-
     **MAP_OLD,
+
+    **MAP_PERMANENT,
 
     **MAP_2019,
 
---
+++ urls/_permanent.py
@@ -0,0 +1,20 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016-2020, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+# flake8: noqa
+
+MAP_PERMANENT = {
+    'distro/core/aws-amis.txt':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/distro/core/aws-amis.txt',
+    'distro/core/virtualbox-images.txt':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/distro/core/virtualbox-images.txt',
+
+    # Link Shortening
+    'a_diversity_metrics':
+        'http://scikit-bio.org/docs/latest/generated/skbio.diversity.alpha',
+}
